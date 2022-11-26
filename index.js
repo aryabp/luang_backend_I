129b0e9b28e860d169e4e522c5bd0ff29a9458bf
A. Bagaskara P <aryabagasprox@gmail.com>
2022-11-26 11:13:35 +0700

    s

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.use('/',userRouter)
