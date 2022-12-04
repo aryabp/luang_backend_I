fa14d2cae833def28899c68cc76ab357454a30d6
A. Bagaskara P <aryabagasprox@gmail.com>
2022-12-04 19:45:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors({
     origin: [
-      '*'
+      'http://localhost:3000',
+      'https://modul-17-2-arya-bagaskara-pratama-3tul64myb-aryabp.vercel.app'
     ],
     credentials: true,
     exposedHeaders: ['set-cookie']
