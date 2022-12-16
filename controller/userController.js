12fd4e7772a66afc3426b1b182365c02f53810cb
A. Bagaskara P <aryabagasprox@gmail.com>
2022-12-16 23:36:50 +0700

    update services

--- controller/userController.js
+++ controller/userController.js
@@ -1,34 +1,32 @@
-const express = require('express')
-const db = require('../db.config/db.config')
 const jwt = require('jsonwebtoken')
 //const Auth = require('../middleware/auth')
-//const cookieParser = require('cookie-parser')
 require("dotenv").config()
 const bcrypt = require('bcrypt')
+const nodemailer = require('nodemailer')
+const otplib = require('otplib')
+const { urlServices } = require('../services')
 SECRET = process.env.SECRET
-const axios = require('axios')
-const nodemailer = require('nodemailer');
-const otplib = require('otplib');
 
 const register = async (req, res, next) => {
     const { username, email, password } = req.body
     try {
         const hash = await bcrypt.hash(password, 10)
-        await db.query(`insert into users values (DEFAULT, $1, $2, $3,'pembeli')`, [email, username, hash])
+        await urlServices.register(username, email, hash)
         res.status(200).send('Data has been updated')
     } catch (err) {
-        if (err.code == "23505") { res.status(401).send(`username or email already taken, take another`) } else {
+        if (err.code == "23505") {
+            res.status(401).send(`username or email already taken, take another`)
+        }
+        else {
             res.status(401).send(`Invalid Input, require "username","email", "password","status">>'pembeli'`)
         }
-
     }
 }
 
 const login = async (req, res, next) => {
     const { email, password } = req.body;
-    try {
-        const data = await db.query(`SELECT * from users WHERE email = $1`, [email])
-        const user = data.rows
+    try {        
+        const user = await urlServices.login(email)
         if (user.length == 0) {
             res.status(400).json({
                 error: "User is not registered, Sign Up first"
@@ -67,17 +65,15 @@ const login = async (req, res, next) => {
         res.status(500).json({
             error: "Database error occurred while signing in!"
         })
-    }
+    }   
 }
 
 const logout = async (req, res, next) => {
     try {
         return res.clearCookie('JWT').status(200).send('Logout Successfully')
     } catch (err) {
-        console.log(err.message)
         return res.status(500).send(err)
     }
-
 }
 
 const verify = async (req, res, next) => {
@@ -85,25 +81,20 @@ const verify = async (req, res, next) => {
         const { token } = req.body
         const verified = jwt.verify(token, SECRET)
         return res.status(200).json(verified)
-
     } catch (err) {
-        console.log(err.message)
         return res.status(500).json({ error: err })
     }
 }
 const otp = async (req, res, next) => {
-
     const { email } = req.body
     if (email != undefined) {
-        const lihat = await db.query(`SELECT * from users where email='${email}'`)
+        const lihat = await urlServices.otp1
         if (lihat.rowCount != 0) {
             const secret = otplib.authenticator.generateSecret();
 
             const token = otplib.totp.generate(secret);
-
-            await db.query(`DELETE FROM otp where email='${email}'`)
-            await db.query(`INSERT INTO otp values('${email}','${secret}','${token}')`)
-
+            await urlServices.otp2
+            
             let transporter = nodemailer.createTransport({
                 host: process.env.SERVICE_SMTP,
                 port: process.env.SERVICE_PORT,
@@ -139,7 +130,7 @@ const otp = async (req, res, next) => {
 const ubah = async (req, res, next) => {
     const { username, password, token } = req.body
     if (token != undefined && token != '') {
-        const data = await db.query(`select email from otp where token='${token}'`)
+        const data = await urlServices.ubah1(token)
         if (data.rows != '') {
             try {
                 const email = data.rows[0].email
@@ -155,9 +146,7 @@ const ubah = async (req, res, next) => {
                     x += `password = '${hash}' `
                 }
                 x += `WHERE email='${email}'`
-                await db.query(x)
-                await db.query(`DELETE FROM otp where token='${token}'`)
-                const user = await db.query(`SELECT * from users WHERE email = $1`, [email])
+                const user = await urlServices.ubah2(x,token,email)
                 const tokenJWT = jwt.sign({
                     userid: user.rows[0].userid,
                     email: user.rows[0].email,
@@ -184,12 +173,11 @@ const ubah = async (req, res, next) => {
 const hapus = async (req, res, next) => {
     const { token } = req.body
     if (token != undefined && token != '') {
-        const data = await db.query(`select email from otp where token='${token}'`)
+        const data = await urlServices.hapus1(token)
         if (data.rows != '') {
             try {
                 const email = data.rows[0].email
-                await db.query(`DELETE FROM users where email='${email}'`)
-                await db.query(`DELETE FROM otp where token='${token}'`)
+                await urlServices.hapus2(email,token)
                 res.send('data berhasil dihapus')
             } catch (err) {
                 res.send(err)
@@ -206,7 +194,7 @@ const lupa = async (req, res, next) => {
     const { token } = req.body
     if (token != undefined && token != '') {
         try {
-            const data = await db.query(`select  * from users INNER JOIN  otp  ON users.email=otp.email where otp.token='${token}'`)
+            const data = await urlServices.lupa(token)
             const tokenJWT = jwt.sign({
                 userid: data.rows[0].userid,
                 email: data.rows[0].email,
@@ -228,30 +216,24 @@ const lupa = async (req, res, next) => {
     }
 }
 
-const getcategory = async (req,res,next) =>{
-    try{
-        const lihat = await db.query(`SELECT * from category`)
+const getcategory = async (req, res, next) => {
+    try {
+        const lihat = await urlServices.getcategory()
+        console.log(lihat)
         res.send(lihat.rows)
-    }catch(err){
+    } catch (err) {
         res.send(err)
     }
 }
-
-const getproduct = async (req,res,next) =>{
+const getproduct = async (req, res, next) => {
     try{
         const {id} = req.body
-        let query = `SELECT * from ((product a INNER JOIN profile b ON a.profileid = b.profileid ) INNER JOIN category c ON a.categoryid = c.categoryid)`
-        if (id != undefined && ''){
-            query+= ` WHERE a.product = ${id}`
-        }
-        const product = await db.query(query)
+        const product = await urlServices.getproduct(id)
         res.send(product.rows)
     }catch(err){
         res.send(err)
     }
 }
-
-
 
 module.exports = {
     register,
--- services/url.services.js
+++ services/url.services.js
@@ -1,5 +1,59 @@
+const db = require('../db.config/db.config')
 
-const register = async () =>{
-
+const register = async (username, email, hash) => {
+    await db.query(`insert into users values (DEFAULT, $1, $2, $3,'pembeli')`, [email, username, hash])
 }
-module.exports={register}
+const login = async (email) => {
+    const data = await db.query(`SELECT * from users WHERE email = $1`, [email])
+    return data.rows
+}
+const otp1 = async (email) => {
+    return db.query(`SELECT * from users where email='${email}'`)
+}
+const otp2 = async (email, secret, token) => {
+    await db.query(`DELETE FROM otp where email='${email}'`)
+    await db.query(`INSERT INTO otp values('${email}','${secret}','${token}')`)
+}
+const ubah1 = async (token) => {
+    return db.query(`select email from otp where token='${token}'`)
+}
+const ubah2 = async (x, token, email) => {
+    await db.query(x)
+    await db.query(`DELETE FROM otp where token='${token}'`)
+    return await db.query(`SELECT * from users WHERE email = $1`, [email])
+}
+const hapus1 = async (token) => {
+    return await db.query(`select email from otp where token='${token}'`)
+}
+const hapus2 = async (email, token) => {
+    await db.query(`DELETE FROM users where email='${email}'`)
+    await db.query(`DELETE FROM otp where token='${token}'`)
+}
+const lupa = async (token) => {
+    return await db.query(`select  * from users INNER JOIN  otp  ON users.email=otp.email where otp.token='${token}'`)
+}
+const getcategory = async () => {
+    const x = await db.query(`SELECT * from category`)
+    return x
+}
+const getproduct = async (id) => {
+    let query = `SELECT * from ((product a INNER JOIN profile b ON a.profileid = b.profileid ) INNER JOIN category c ON a.categoryid = c.categoryid)`
+    if (id != undefined && '') {
+        query += ` WHERE a.product = ${id}`
+    }
+    const product = await db.query(query)
+    return product
+}
+module.exports = {
+    register,
+    login,
+    otp1,
+    otp2,
+    ubah1,
+    ubah2,
+    hapus1,
+    hapus2,
+    lupa,
+    getcategory,
+    getproduct
+}
